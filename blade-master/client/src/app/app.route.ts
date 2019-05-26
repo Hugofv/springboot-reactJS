70a847e463de24e5e0815df9b47b2dbe03bdfb05
Hugo Fernandes <hugo_fernandes2010@hotmail.com>
2019-05-26 18:23:57 -0300

    Commit inicial

--- blade-master/client/src/app/app.route.ts
+++
@@ -1,8 +0,0 @@
-import { Routes, ActivatedRoute } from '@angular/router'
-import { CreateComponent } from './sistema/create/create.component';
-import { SistemaComponent } from './sistema/sistema.component';
-
-export const ROUTES: Routes = [
-  {path: '', component: SistemaComponent},
-  {path: 'sistema', component: CreateComponent}
-]
--- blade-master/client/src/app/models/sistema.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Sistema {
-  id: Int16Array;
-  descricao: string;
-  sigla: string;
-  email: string;
-  url: string;
-}
